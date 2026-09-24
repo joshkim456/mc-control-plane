c7b6553f92f727ef6dfc311c928d2021985e838b
joshkim456 <joshkim2028@gmail.com>
2026-09-24 23:43:24 +0100

    Parse returns an error, main reads testdata/server.properties

--- cmd/sleepy/main.go
+++ cmd/sleepy/main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/joshkim456/mc-control-plane/internal/properties"
+	"os"
 )
 
 func main() {
 	fmt.Println("sleepy: starting")
-	fmt.Println(properties.Parse())
+
+	file, fileOpenErr := os.Open("testdata/server.properties")
+	if fileOpenErr != nil {
+		fmt.Println("Error:", fileOpenErr)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	props, parseErr := properties.Parse(file)
+
+	if parseErr != nil {
+		fmt.Println("Error:", parseErr)
+		os.Exit(1)
+	}
+
+	maxPlayers, ok := props.Get("max-players")
+	fmt.Println(maxPlayers, ok)
 }
--- internal/properties/properties.go
+++ internal/properties/properties.go
@@ -1,5 +1,16 @@
 package properties
 
-func Parse() string {
-    	return "parsed"
+import "io"
+
+type Properties struct {
+	values map[string]string
+}
+
+func (p Properties) Get(key string) (string, bool) {
+	value, ok := p.values[key]
+	return value, ok
+}
+
+func Parse(r io.Reader) (Properties, error) {
+	return Properties{values: map[string]string{"max-players": "10"}}, nil
 }
